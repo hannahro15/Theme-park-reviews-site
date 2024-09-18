c317951aa360b54210e55f77f66535a9f80b5b16
Hannah Olbrich <hannaholbrich@gmail.com>
2024-09-18 07:30:13 +0000

    Amend 404 error page route

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/404")
-def error_page():
-    return render_template("404.html")
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
 @app.route("/get_rides")
